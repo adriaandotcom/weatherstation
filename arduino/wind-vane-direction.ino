95547caed8a81176648bd82066a1be02ca66aadd
Adriaan <adriaanvanrossum@users.noreply.github.com>
2021-11-20 14:10:58 +0100

    Update code

--- arduino/wind-vane-direction.ino
+++ arduino/wind-vane-direction.ino
@@ -1,26 +1,31 @@
-int LastValue;
+int lastRead;
+int lastTime = millis();
+
 
 void setup() {
-  LastValue = 1;
+  lastRead = 1;
   Serial.begin(115200);
 }
 
 void loop() {
-  int VaneValue = analogRead(33);
+  int read = analogRead(33);
 
-  // Only update the display if change greater than 2 degrees.
-  if (abs(VaneValue - LastValue) > 70)
-  {
-    Serial.print(VaneValue);
+  // Only update with substantial change
+  if (abs(read - lastRead) > 70 || millis() - lastTime > 5000) {
+    Serial.print(read);
     Serial.print("\t");
-    getHeading(VaneValue);
-    LastValue = VaneValue;
+
+    const char * cardinalDirection = getCardinalDirection(read);
+    Serial.println(cardinalDirection);
+    
+    lastRead = read;
+    lastTime = millis();
   }
 
   delay(500);
 }
 
-
+// Measurements manually taken
 int NE = 2887;
 int NW = 3051;
 int SE = 3183;
@@ -30,30 +35,32 @@ int E  = 3526;
 int S  = 3810;
 int W  = 3906;
 
+// When number is in the middle of two numbers
 int middle(int start, int end) {
   return min(start, end) + abs(start - end) / 2;
 }
 
-// Converts compass direction to heading
-void getHeading(int VaneValue) {
-  if (VaneValue < middle(NE - 200, NE))
-    Serial.println("TOO LOW");
-  else if (VaneValue < middle(NE, NW))
-    Serial.println("NE");
-  else if (VaneValue < middle(NW, SE))
-    Serial.println("NW");
-  else if (VaneValue < middle(SE, N))
-    Serial.println("SE");
-  else if (VaneValue < middle(N, SW))
-    Serial.println("N");
-  else if (VaneValue < middle(SW, E))
-    Serial.println("SW");
-  else if (VaneValue < middle(E, S))
-    Serial.println("E");
-  else if (VaneValue < middle(S, W))
-    Serial.println("S");
-  else if (VaneValue < middle(W, W + 200))
-    Serial.println("W");
+// Converts resistance to cardinal direction
+// Wind heading means the direction where the wind is going to
+const char* getCardinalDirection(int value) {
+  if (value < middle(NE - 200, NE))
+    return "";
+  else if (value < middle(NE, NW))
+    return "NE";
+  else if (value < middle(NW, SE))
+    return "NW";
+  else if (value < middle(SE, N))
+    return "SE";
+  else if (value < middle(N, SW))
+    return "N";
+  else if (value < middle(SW, E))
+    return "SW";
+  else if (value < middle(E, S))
+    return "E";
+  else if (value < middle(S, W))
+    return "S";
+  else if (value < middle(W, W + 200))
+    return "W";
   else
-    Serial.println("TOO HIGH");
+    return "";
 } 
